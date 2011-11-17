8fa2290ef39ab1c8af7354ef02d8940b921ecc10
Jack Mitchell <jack@communistcode.co.uk>
2011-11-17 11:47:20 +0000

    initial commit

---
+++ templates/corePage.inc.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	
+	<head>
+
+		<!-- Google font API -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600' rel='stylesheet' type='text/css'>
+
+		<!-- Page data -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><?php echo($title." : ".$pageContent['title']); ?></title>
+		
+		<!-- Stylesheets -->
+		<link href="themes/<?php echo($pageTools->getTheme("base")); ?>/stylesheets/base.css" rel="stylesheet" type="text/css" />
+	
+	</head>
+
+	<body>
+		<div id="mainContainer">
+			<div id="title">
+				<?php 
+					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/title.inc.php"); 
+				?>
+			</div>
+			<div id='navBar'>
+				<?php 
+					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/links.inc.php"); 
+				?>
+			</div>
+			<div id="body">
+				<div class='baseBody'>
+					<?php		
+						echo($content);
+					?>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+				<?php 
+					require_once($fullPath."/themes/".$pageTools->getTheme("base")."/templates/footer.inc.php"); 
+				?>
+		</div>
+	</body>
+
+</html>
